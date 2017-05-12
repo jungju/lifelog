221d2cb4f2915a97460ca8e6f5478146b5f86bf4
jungju <leejungju.go@gmail.com>
2017-05-12 23:05:11 +0900

    ui 변경

--- view.go
+++ view.go
@@ -20,19 +20,19 @@ func handlerHomeView(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	token := r.URL.Query().Get("token")
-
-	vaildToken := true
-	_, err = jDB.GetJawbone(token)
-	if err != nil {
-		logrus.WithError(err).Error("Failed GetJawbone")
-		token = ""
-		vaildToken = false
-	}
-
+	vaildToken := false
 	urlToken := ":token"
 	if token != "" {
-		urlToken = token
+		_, err = jDB.GetJawbone(token)
+		if err == nil {
+			vaildToken = true
+			urlToken = token
+		} else {
+			logrus.WithError(err).Error("Failed GetJawbone")
+			vaildToken = false
+		}
 	}
 
 	var data = map[string]interface{}{
